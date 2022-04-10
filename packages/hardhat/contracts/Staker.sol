ac8552658386e43f863293cf10f0333f5b615f76
necr0z <necroz1998@gmail.com>
2022-04-10 13:53:18 +0800

    init staking function

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -1,31 +1,45 @@
-pragma solidity >=0.6.0 <0.7.0;
+pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
 
+
+/**
+* @title Stacker Contract
+* @author scaffold-eth
+* @notice A contract that allow users to stack ETH
+*/
 contract Staker {
 
+  // External contract that will old stacked funds
   ExampleExternalContract public exampleExternalContract;
 
+  // Balances of the user's stacked funds
+  mapping(address => uint256) public balances;
+
+  // Staking threshold
+  uint256 public constant threshold = 1 ether;
+
+  // Contract's Events
+  event Stake(address indexed sender, uint256 amount);
+
+  /**
+  * @notice Contract Constructor
+  * @param exampleExternalContractAddress Address of the external contract that will hold stacked funds
+  */
   constructor(address exampleExternalContractAddress) public {
-      exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
+    exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
 
-  // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
-  //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
-
-
-  // After some `deadline` allow anyone to call an `execute()` function
-  //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
-
-
-  // if the `threshold` was not met, allow everyone to call a `withdraw()` function
-
-
-  // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
-
-
-  // Add the `receive()` special function that receives eth and calls stake()
-
+  /**
+  * @notice Stake method that update the user's balance
+  */
+  function stake() public payable {
+    // update the user's balance
+    balances[msg.sender] += msg.value;
+    
+    // emit the event to notify the blockchain that we have correctly Staked some fund for the user
+    emit Stake(msg.sender, msg.value);
+  }
 
 }
