26abec09fabc28b9132d69c0fcad4231cfe84348
Kai Xun <kaixun_oei@mymail.sutd.edu.sg>
2022-04-16 23:45:57 +0800

    Bugged Deploy Contract

--- src/pages/RoleSel.jsx
+++ src/pages/RoleSel.jsx
@@ -5,29 +5,41 @@ import WalletBalance from '../components/WalletBalance'
 import Install from '../components/Install';
 import { Routes, Route, Link } from 'react-router-dom';
 
-
 function RoleSel() {
+  function submitSeller() {
+    var SellerAddr = document.getElementById("Address").value;
+    localStorage.setItem("SellerAddr",SellerAddr);
+    console.log("Seller Addr set");
+  }
+
+  function submitBuyer() {
+    var BuyerAddr = document.getElementById("Address").value;
+    localStorage.setItem("BuyerAddr",BuyerAddr);
+    console.log("Buyer Addr set");
+  }
+
   return (
     <>
-    <h1 style={{position:"absolute",top:"10%"}}>Role Selection</h1>
-    <div className="" style={{width:"30vw",marginRight:"5vw", position: "absolute", top:"206px",left:"207px"}}>
+    <h1 style={{position:"absolute",top:"5%"}}>Role Selection</h1>
+    <div className="" style={{width:"30vw",marginRight:"5vw", position: "absolute", top:"20%",left:"207px"}}>
       <h2> Buyer </h2>
       <p>
         Fulfills the contract to obtain the money
       </p>
       
     </div>
-    <div className="" style={{width:"30vw",marginRight:"5vw", position: "absolute", top:"206px",left:"850px"}}>
+    <div className="" style={{width:"30vw",marginRight:"5vw", position: "absolute", top:"20%",left:"850px"}}>
       <h2> Seller </h2>
       <p>
         Pays the buyer after contract is fulfilled
       </p>
       
     </div>
+    <div className="flexbox" style={{position:"absolute",top:"40%"}}>Your Address <input className="GreyInputNarrow" id="Address" /></div>
     <h2 style={{marginTop:"20%"}}> Are you the buyer or the seller?</h2>
     <div>
-      <button className="BlueButton" style={{marginRight: "5vw"}}>Buyer</button>
-      <button className="BlueButton" style={{marginLeft: "5vw"}}>Seller</button>
+      <button className="BlueButton" style={{marginRight: "5vw"}} onClick={submitBuyer}>Buyer</button>
+      <button className="BlueButton" style={{marginLeft: "5vw"}} onClick={submitSeller}>Seller</button>
     </div>
     </>
   )
--- src/pages/TermsConfirm.jsx
+++ src/pages/TermsConfirm.jsx
@@ -8,11 +8,22 @@ import React from "react";
 
 
 class TermsConfirm extends React.Component {
+  async deployContract() {
+    const buyerAddr = localStorage.getItem("buyerAddr");
+    const sellerAddr = localStorage.getItem("sellerAddr");
+    const EthAmt = localStorage.getItem("EthAmt");
+    const contractTerms = JSON.parse(localStorage.getItem("terms"));
+    const EscrowContract = await ethers.getContractFactory("EscrowContract",wallet=wallet);
+    const contract = await EscrowContract.deploy(
+      buyerAddr, //'0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266', // Buyer
+      sellerAddr, //'0x70997970c51812dc3a010c7d01b50e0d17dc79c8', // Seller (Same as buyer address to trick the hardhat into thinking we are the seller)
+      EthAmt, //'200000000000000000' // value in wei (1 eth = 1*10^18 wei)0
+      contractTerms
+    );
+    console.log("Contract deployed between:",contract.buyerAddr," and ",contract.sellerAddr);
+  }
 
   render() {
-
-
-
   var dealID="ABCDEFGDHIJKLMNK";
   var contractTerms = JSON.parse(localStorage.getItem("terms"));
   var contractList = contractTerms.map((contractTerm,index) =>
@@ -30,7 +41,7 @@ class TermsConfirm extends React.Component {
     <input className="GreyInput" id="EthAmt" type="numbers" step="0.01" style={{marginLeft:"150px",height:"30px",width:"100px",color:"black"}} value={localStorage.getItem("EthAmt")} readOnly /> Eth
     </div>
     <h3>Press review the contract.</h3>
-    <button className="BlueButton" style={{position:"absolute",bottom:"5vh",left:"12vw",width:"34vw",height:"8vh"}}>Accept Contract</button>
+    <button className="BlueButton" style={{position:"absolute",bottom:"5vh",left:"12vw",width:"34vw",height:"8vh"}} onClick={this.deployContract}>Accept Contract</button>
     <button className="BlueButton" style={{position:"absolute",bottom:"5vh",right:"12vw",width:"34vw",height:"8vh"}}>Decline Contract</button>
     </>
   )
