c60e22ea817494e9d2274aa42812ed744665edf5
Kai Xun <kaixun_oei@mymail.sutd.edu.sg>
2022-04-17 20:33:53 +0800

    pushing for cawin

--- src/pages/Staking.jsx
+++ src/pages/Staking.jsx
@@ -4,18 +4,105 @@ import '../App.css'
 import WalletBalance from '../components/WalletBalance'
 import Install from '../components/Install';
 import { Routes, Route, Link } from 'react-router-dom';
-import { ethers } from 'ethers';
+import { Contract, ethers } from 'ethers';
+import Web3 from 'web3/dist/web3.min.js'
+
 
 
 function Staking() {
   var StakeAmt = 1;
   var StakeTotal = 10;
-  var address="0x6b87509a2d41f2194f694535852Ea094c24080C3"
+  var address="0x6b87509a2d41f2194f694535852Ea094c24080C3";
+  var abi = [
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "sender",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "Stake",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "balances",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getStakers",
+      "outputs": [
+        {
+          "internalType": "address[]",
+          "name": "",
+          "type": "address[]"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "stake",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "stakers",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "withdraw",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ]
 
   async function stake() {
-    const Staker = await ethers.Contract(address);
-    const staker = await Staker.attach(address);
-    staker.stake();
+    const Staker = await Web3.eth.Contract(abi,address);
+    Staker.methods.stake().call();
     console.log("Staked");
   }
 
